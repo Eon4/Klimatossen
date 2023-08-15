8173b71de393b745967e0c0b4e5225d8fa9c3933
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-15 13:35:36 +0200

    working on it

--- klimatossen/src/components/header.jsx
+++ klimatossen/src/components/header.jsx
@@ -7,7 +7,7 @@ export function Header(){
             <h1 className={style.title}>Klima<span>tossen</span></h1>
             <img className={style.Headerimg} src={HeaderImg} alt="header" />
             <h2 className={style.smalltitle}>KLIMA-TOSSEN.DK</h2>
-            <p>"Det er rigtigt, at vi hver især kun kan bidrage lidt
+            <p className={style.headertext}>"Det er rigtigt, at vi hver især kun kan bidrage lidt
 til den samlede løsning"
 
 "Lige så rigtigt er det, at vi slet ikke kan nå målet,
