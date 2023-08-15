46910919b6416f8895511dc3f968a3ca0e6f7de7
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-15 14:28:06 +0200

    header styled

--- klimatossen/src/components/header.jsx
+++ klimatossen/src/components/header.jsx
@@ -7,11 +7,12 @@ export function Header(){
             <h1 className={style.title}>Klima<span>tossen</span></h1>
             <img className={style.Headerimg} src={HeaderImg} alt="header" />
             <h2 className={style.smalltitle}>KLIMA-TOSSEN.DK</h2>
-            <p className={style.headertext}>"Det er rigtigt, at vi hver især kun kan bidrage lidt
-til den samlede løsning"
-
-"Lige så rigtigt er det, at vi slet ikke kan nå målet,
-uden at hver især bidrager en smule til det"</p>
+            <p className={style.headertext}>
+                "Det er rigtigt, at vi hver især kun kan bidrage lidt<br/>
+                til den samlede løsning."<br/>
+                Lige så rigtigt er det, at vi slet ikke kan nå målet,<br/>
+                uden at hver især bidrager en smule til det"
+            </p>
         </header>
     )
 }
