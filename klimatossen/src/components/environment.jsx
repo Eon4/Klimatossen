d3883d10e43ef846431165bcc7606124c1f24691
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-16 11:13:33 +0200

    climate section done

--- klimatossen/src/components/environment.jsx
+++ klimatossen/src/components/environment.jsx
@@ -1,43 +1,34 @@
 import style from "../style/enviroment.module.scss";
+import climate from "../assets/images/icon-Climate.png";
+
 
 export function Environment() {
+  const text = `Kort om klimaet og miljøet?
+
+  - Klima og miljø hænger sammen.
+  - Klimaforandringer påvirker miljøet og omvendt.
+  - Miljøet kan betragtes som en organisme, og er det vi baserer vores liv og overlevelse på, hvis vi ændrer i miljøet, har det oftest en dominoeffekt, der påvirker alle dele i miljøet.
+  - Ændringer i atmosfærens CO2 og Ozonlag påvirker miljøet på hele kloden, med ændringer i temperatur og nedbør som følge.
+  - Ændrede temperaturer og nedbørsmønster påvirker ikke kun kældre og infrastruktur, men også biodiversiteten som planter insekter mm.
+  - Din adfærd påvirker klimaet i størrer eller mindre grad.`;
+
+  const lines = text.split("\n");
+
   return (
-    <section className={style.co2}>
-      <article>
-        <h2>KLIMA OG MILJØ?</h2>
-        <p>
-        Kort om klimaet og miljøet?
-
-- Klima og miljø hænger sammen.
-
-- Klimaforandringer påvirker miljøet og omvendt
-
-.
-- Miljøet kan betragtes som en organisme, og er det vi baserer vores liv og overlevelse på, hvis vi ændrer i miljøet, har det oftest en dominoeffekt, der påvirker alle dele i miljøet.
-- Ændringer i atmosfærens CO2 og Ozonlag påvirker miljøet på hele kloden, med ændringer i temperatur og nedbør som følge.
-
-- Ændrede temperaturer og nedbørsmønster påvirker ikke kun kældre og infrastruktur, men også biodiversiteten som planter insekter mm.
-- Din adfærd påvirker klimaet i størrer eller mindre grad.
-
-
-HVORDAN KAN JEG BIDRAGE?
-Hvordan belaster du miljøet mindst muligt i dagligdagen?
-
-- Dit forbrug overordnet herunder din produktion af affald, bidrager til udledning af CO2
-
-- Fødevareproduktion har stor indflydelse på CO2 balancen, tænk over
- dit valg i dagligdagen.
-- Din bolig og den måde du har indrettet dig påvirker miljøet.
-- Jordens skove og verdenshavene er den største "producent" af den
- ilt, der er nødvendig, for liv på Jorden, som vi kender det.
-
-- Mennesket er den enlige faktor der har den største indflydelse på
- CO2 -balancen på Jorden i dag!
-
-	- Du kan påvirke CO2 balancen, både ved at ændre små ting i din
- hverdag, eller ved aktivt deltage i kampen for et bedre miljø.
-        </p>
-    <a>Læs mere</a>
+    <section className={style.climate}>
+      <h2>KLIMA OG MILJØ?</h2>
+      <article className={style.sections}>
+        <div className={style.imageSection}>
+          <img src={climate} alt="climate" />
+        </div>
+        <div className={style.textSection}>
+          {lines.map((line, index) => (
+            <p key={index} className={index === 0 ? style.important : ''}>
+              {line}
+            </p>
+          ))}
+          <a>Læs mere</a>
+        </div>
       </article>
     </section>
   );
