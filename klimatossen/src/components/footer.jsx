15c88357973acb55602edfb8a13ba12854b789c6
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-16 13:16:06 +0200

    footer update

--- klimatossen/src/components/footer.jsx
+++ klimatossen/src/components/footer.jsx
@@ -1,54 +1,46 @@
 import style from "../style/footer.module.scss";
-// import iconface from '../assets/images/facelogo.svg';
-// import iconlinked from '../assets/images/linkedinlogo.svg';
-
 
 
 export function Footer() {
   return (
-    <footer className={style.autoFlow}>
-      <article>
-        <h2>KlimaTossen NPO</h2>
-        <p>Find os her:</p>
+    <footer>
+      <div>
+      <section className={style.npo}>
+        <h5>KlimaTossen NPO</h5>
         <p>
-        Vesterbrogade 92
-9000 Aalborg
-info@klima-tossen.dk
-9922 3344 (Åben man - fre: 08.00 - 16.00)
+          Vesterbrogade 92 <br /> 9000 Aalborg <br /> info@klima-tossen.dk{" "}
+          <br /> 9922 3344 (Åben man - fre: 08.00 - 16.00)
         </p>
-      </article>
-      <article>
-        <h2>SAMARBEJDSPARTNERE:</h2>
+        <h5>SAMARBEJDSPARTNERE:</h5>
         <p>
-https://www.dn.dk/
-http://www.miljoeportal.dk
+          <ul>
+            <li>
+              <a href="https://www.dn.dk/">https://www.dn.dk/</a>
+            </li>
+            <li>
+              <a href="http://www.miljoeportal.dk">
+                http://www.miljoeportal.dk
+              </a>
+            </li>
+          </ul>
         </p>
-        <h1>KLIMATOSSEN</h1>
-      </article>
-      <article>
-        <h2>Ja tak! Jeg vil gerne høre mere.</h2>
-      </article>
-      <form action="Thanks.html" novalidate onsubmit="return validate(this);" class="myform">
-        <fieldset id="signup">
-          {/* <legend>Signup Form</legend> */}
-            <input type="text" id="fname" name="fname" placeholder="Fulde navn" required />
-
-            <input type="text" id="city" name="city" placeholder="Hjem by" required />
-
-            <input type="email" id="email" name="email" placeholder="Email Address" required />
-
-            <input type="checkbox" id="talk" name="talk" value="letstalk"/>
-            <label for="talk">Få en samtale med Klimatossen</label>
-
-            
-            <input type="checkbox" id="news" name="news" value="getnews"/>
-            <label for="talk">Modtag Nyhedsbrev</label>
-
-            <input type="tel" id="phone" name="phone" placeholder="Telefon nummer" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"></input>
-        
-            <input type="submit" value="Submit" />
-      </fieldset>
-      </form>
+        <h1>Klima<span>tossen</span></h1>
+      </section>
+      <section className={style.tilmeld}>
+        <h5>Ja tak! Jeg vil gerne hører mere.</h5>
+        <form>
+          <input type="text" placeholder="Fulde navn" />
+          <input type="text" placeholder="Hjem by" />
+          <input type="email" placeholder="Email adresse" />
+          <input type="tel" placeholder="Telefon nummer" />
+          <label htmlFor="">Få en samtale med Klimatossen </label>
+          <input className={style.checkbox} type="checkbox" />
+          <label htmlFor="">Modtag Nyhedsbrev</label>
+          <input className={style.checkbox} type="checkbox" />
+          <input type="Submit" value="Send" />
+        </form>
+      </section>
+      </div>
     </footer>
   );
 }
