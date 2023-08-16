cb3f333412b21a659f9903b814b47fcecd0c8399
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-16 11:27:40 +0200

    arrangement section done

--- klimatossen/src/components/arrangement.jsx
+++ klimatossen/src/components/arrangement.jsx
@@ -1,26 +1,38 @@
 import style from "../style/arrangement.module.scss";
+import arr from "../assets/images/icon-Arrangement.png";
+
 
 export function Arrangement() {
+  const text = `Deltag i et af vores arrangementer og gør en forskel!
+
+  - “Combing” (”at fjerne affald”, f.eks. beachcombing: , rense stranden for affald og elementer der ikke hører hjemme her).  Invester en dag med familien i naturen og sammen med andre.
+  - Tilmeld dig eet arrangement i dit område.
+  - Tag ansvar hjælp dit nærområde og os alle sammen generelt.
+  - Mød nye mennesker omkring en fælles opgave.
+  - Tag famillien med på en dag for naturen og vores fremtid..
+  - Tilmeld dig eet af vore arrangementer, eller modtag
+   information om Klimatossen!
+  - Hent mere information på siden “Tilmeld Dig”.
+  
+`;
+
+  const lines = text.split("\n");
+
   return (
-    <section className={style.co2}>
-      <article>
-        <h2>DELTAG I ARRANGEMENT</h2>
-        <p>
-Deltag i eet af vores arrangementer og gør en forskel!
-
-- “Combing” (”at fjerne affald”, f.eks. beachcombing: , rense stranden for affald og elementer der ikke hører hjemme her).  Invester en dag med familien i naturen og sammen med andre.
-- Tilmeld dig eet arrangement i dit område
-.
-- Tag ansvar hjælp dit nærområde og os alle sammen generelt.
-- Mød nye mennesker omkring en fælles opgave.
-- Tag famillien med på en dag for naturen og vores fremtid..
-- Tilmeld dig eet af vore arrangementer, eller modtag
- information om Klimatossen!
-
-
-- Hent mere information på siden “Tilmeld Dig”.
-        </p>
-    <a>Læs mere</a>
+    <section className={style.arrangement}>
+      <h2>DELTAG I ARRANGEMENT</h2>
+      <article className={style.sections}>
+        <div className={style.imageSection}>
+          <img src={arr} alt="arr" />
+        </div>
+        <div className={style.textSection}>
+          {lines.map((line, index) => (
+            <p key={index} className={index === 0 ? style.important : ''}>
+              {line}
+            </p>
+          ))}
+          <a>Læs mere</a>
+        </div>
       </article>
     </section>
   );
